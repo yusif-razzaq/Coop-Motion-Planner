e071f67cabcfa6343b65b964d3a8523b5c36b6f2
Yusif <yusifrazz@gmail.com>
2023-11-24 16:25:33 -0700

    more compact

--- includes/GoalRegionDatabase.h
+++ includes/GoalRegionDatabase.h
@@ -44,13 +44,9 @@ class GoalRegion2ndOrderCar: public ob::GoalRegion
 {
 public:
     GoalRegion2ndOrderCar(const ob::SpaceInformationPtr &si, double gx, double gy): 
-        ob::GoalRegion(si), gx_(gx), gy_(gy)
-    {
-        threshold_ = 0.5;
-    }
+        ob::GoalRegion(si), gx_(gx), gy_(gy) { threshold_ = 0.5; }
     
-    double distanceGoal(const ob::State *st) const override
-    {
+    double distanceGoal(const ob::State *st) const override {
         const double* robot_pos = st->as<ob::CompoundStateSpace::StateType>()->as<ob::RealVectorStateSpace::StateType>(0)->values;
         return sqrt(pow(robot_pos[0] - gx_, 2) + pow(robot_pos[1] - gy_, 2));
     }
--- includes/World.h
+++ includes/World.h
@@ -68,98 +68,87 @@ struct Obstacle {
 
 // An agent has a name, shape, dynamics, start and goal regions
 // Created as class to keep important variables safe
-class Agent
-{
-public:
-    Agent(std::string name, std::string dyn, std::vector<double> shape, std::vector<double> s, std::vector<double> g) {
-        name_ = name;
-        dynamics_ = dyn;
-        shape_ = shape;
-        start_ = s;
-        goal_ = g;
-    }
-    std::string getName() const {return name_;};
-    std::string getDynamics() const {return dynamics_;};
-    std::vector<double> getShape() const {return shape_;};
-    std::vector<double> getStartLocation() const {return start_;};
-    std::vector<double> getGoalLocation() const {return goal_;};
-private:
-    std::string name_;
-    std::string dynamics_;
-    std::vector<double> shape_;
-    std::vector<double> start_;
-    std::vector<double> goal_;
+class Agent {
+    public:
+        Agent(std::string name, std::string dyn, std::vector<double> shape, std::vector<double> s, std::vector<double> g) {
+            name_ = name;
+            dynamics_ = dyn;
+            shape_ = shape;
+            start_ = s;
+            goal_ = g;
+        }
+        std::string getName() const {return name_;};
+        std::string getDynamics() const {return dynamics_;};
+        std::vector<double> getShape() const {return shape_;};
+        std::vector<double> getStartLocation() const {return start_;};
+        std::vector<double> getGoalLocation() const {return goal_;};
+    private:
+        std::string name_;
+        std::string dynamics_;
+        std::vector<double> shape_;
+        std::vector<double> start_;
+        std::vector<double> goal_;
 };
 
 
 
 // world class holds all relevent data in the world that is used by OMPL
-class World
-{
-public:
-    World(){}
-    // methods for dimensions
-    void setWorldDimensions(int x, int y){xDim_ = x; yDim_ = y;};
-    std::vector<double> getWorldDimensions() const {return {xDim_, yDim_};};
-    void printWorldDimensions(){OMPL_INFORM("Space Dimensions: [%0.2f, %0.2f]", xDim_, yDim_);}
-    // methods for obstacles
-    void addObstacle(Obstacle obs){Obstacles_.push_back(obs);};
-    std::vector<Obstacle> getObstacles() const {return Obstacles_;};
-    // methods for agents
-    void addAgent(Agent *a){Agents_.push_back(a);};
-    std::vector<Agent*> getAgents() const {return Agents_;};
-    // printing methods for usability
-    void printObstacles()
-    {
-        OMPL_INFORM("%d Obstacle(s) (xMin, yMin, xMax, yMax): ", Obstacles_.size());
-        for (Obstacle o: Obstacles_)
-        {
-            OMPL_INFORM("   - Obstacle: [%0.2f, %0.2f, %0.2f, %0.2f]", o.xMin_, o.yMin_, o.xMax_, o.yMax_);
+class World {
+    public:
+        World(){}
+        // methods for dimensions
+        void setWorldDimensions(int x, int y){xDim_ = x; yDim_ = y;};
+        std::vector<double> getWorldDimensions() const {return {xDim_, yDim_};};
+        void printWorldDimensions(){OMPL_INFORM("Space Dimensions: [%0.2f, %0.2f]", xDim_, yDim_);}
+        // methods for obstacles
+        void addObstacle(Obstacle obs){Obstacles_.push_back(obs);};
+        std::vector<Obstacle> getObstacles() const {return Obstacles_;};
+        // methods for agents
+        void addAgent(Agent *a){Agents_.push_back(a);};
+        std::vector<Agent*> getAgents() const {return Agents_;};
+        // printing methods for usability
+        void printObstacles() {
+            OMPL_INFORM("%d Obstacle(s) (xMin, yMin, xMax, yMax): ", Obstacles_.size());
+            for (Obstacle o: Obstacles_) {
+                OMPL_INFORM("   - Obstacle: [%0.2f, %0.2f, %0.2f, %0.2f]", o.xMin_, o.yMin_, o.xMax_, o.yMax_);
+            }
         }
-    }
-    void printAgents()
-    {
-        OMPL_INFORM("%d Agents: ", Agents_.size());
-        for (Agent *a: Agents_)
-        {
-            OMPL_INFORM("   - Name: %s", a->getName().c_str());
-            OMPL_INFORM("     Dynamics: %s", a->getDynamics().c_str());
-            OMPL_INFORM("     Start: [%0.2f, %0.2f]", a->getStartLocation()[0], a->getStartLocation()[1]);
-            OMPL_INFORM("     Goal: [%0.2f, %0.2f]", a->getGoalLocation()[0], a->getGoalLocation()[1]);
+        void printAgents() {
+            OMPL_INFORM("%d Agents: ", Agents_.size());
+            for (Agent *a: Agents_) {
+                OMPL_INFORM("   - Name: %s", a->getName().c_str());
+                OMPL_INFORM("     Dynamics: %s", a->getDynamics().c_str());
+                OMPL_INFORM("     Start: [%0.2f, %0.2f]", a->getStartLocation()[0], a->getStartLocation()[1]);
+                OMPL_INFORM("     Goal: [%0.2f, %0.2f]", a->getGoalLocation()[0], a->getGoalLocation()[1]);
+            }
         }
-    }
-    void printWorld()
-    {
-        OMPL_INFORM("Map Dimensions: [%0.2f, %0.2f]", getWorldDimensions()[0], getWorldDimensions()[1]);
-        printObstacles();
-        printAgents();
-    }
-private:
-    double xDim_{0};
-    double yDim_{0};
-    std::vector<Obstacle> Obstacles_;
-    std::vector<Agent*> Agents_;
+        void printWorld() {
+            OMPL_INFORM("Map Dimensions: [%0.2f, %0.2f]", getWorldDimensions()[0], getWorldDimensions()[1]);
+            printObstacles();
+            printAgents();
+        }
+    private:
+        double xDim_{0};
+        double yDim_{0};
+        std::vector<Obstacle> Obstacles_;
+        std::vector<Agent*> Agents_;
 };
 
 // function that parses YAML file to world object
-World* yaml2world(std::string file)
-{
+World* yaml2world(std::string file) {
     YAML::Node config;
     World *w = new World();
-    try
-    {
+    try {
         OMPL_INFORM("Path to Problem File: %s", file.c_str());
         config = YAML::LoadFile(file);
         std::cout << "" << std::endl;
         OMPL_INFORM("File loaded successfully. Parsing...");
     }
-    catch (const std::exception& e) 
-    {
+    catch (const std::exception& e) {
         OMPL_ERROR("Invalid file path. Aborting prematurely to avoid critical error.");
         exit(1);
     }
-    try
-    {    
+    try {    
         // grab dimensions from problem definition
         const auto& dims = config["Map"]["Dimensions"];
         const int dimx = dims[0].as<double>();
@@ -168,8 +157,7 @@ World* yaml2world(std::string file)
         
         // set Obstacles
         const auto& obs = config["Map"]["Obstacles"];
-        for (int i=0; i < obs.size(); i++)
-        {
+        for (int i=0; i < obs.size(); i++) {
             std::string name = "obstacle" + std::to_string(i);
             const double minX = obs[name][0].as<double>();
             const double minY = obs[name][1].as<double>();
@@ -194,8 +182,7 @@ World* yaml2world(std::string file)
 
         // Define the agents
         const auto& agents = config["Agents"];
-        for (int i=0; i < agents.size(); i++)
-        {
+        for (int i=0; i < agents.size(); i++) {
             std::string name = "agent" + std::to_string(i);
             const std::vector<double> shape{agents[name]["Shape"][0].as<double>(), agents[name]["Shape"][1].as<double>()};
             const std::vector<double> start{agents[name]["Start"][0].as<double>(), agents[name]["Start"][1].as<double>()};
@@ -206,10 +193,8 @@ World* yaml2world(std::string file)
         OMPL_INFORM("Parsing Complete.");
         std::cout << "" << std::endl;
         w->printWorld();
-
     }
-    catch (const std::exception& e) 
-    {
+    catch (const std::exception& e) {
         OMPL_ERROR("Error During Parsing. Aborting prematurely to avoid critical error.");
         exit(1);
     }
